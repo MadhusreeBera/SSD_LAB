11f368d0d3f4fadedce20febface470a98729ee8
MadhusreeBera <madhusreebera1999@gmail.com>
2022-08-11 15:06:19 +0530

    ssd lab activity 2

---
+++ ssd_lab_activity_2/README.md
@@ -0,0 +1,10 @@
+Q1.
+sample text file name = file.txt
+execution command
+./q1.sh file.txt
+
+if the file does not exist, it will give message accordingly
+
+Q2.
+execution command
+./q2.sh
---
+++ ssd_lab_activity_2/q1.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+if [ -f $1 ]
+then
+    lines=$(cat $1 | wc -l)
+    #echo lines $lines
+    if [ $((lines%2)) -ne 0 ];
+    then
+        lines=$((lines+1))
+    else
+        lines=$lines
+    fi
+    mid=$((lines/2))
+    #echo mid $mid
+
+    cat $1 | head -$mid | tail -1
+else
+    echo "File does not exist"
+fi
